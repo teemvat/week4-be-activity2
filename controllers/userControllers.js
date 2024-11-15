0ca4fb55aaaf73b7de2b6b90d7686c7e17765630
teemvat <teemu.vataja@metropolia.fi>
2024-11-15 12:43:51 +0200

    init

---
+++ controllers/userControllers.js
@@ -0,0 +1,94 @@
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
+
+// GET /users
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({}).sort({ createdAt: -1 });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve users" });
+  }
+};
+
+// POST /users
+const createUser = async (req, res) => {
+  try {
+    const newUser = await User.create({ ...req.body });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: "Failed to create user", error: error.message });
+  }
+};
+
+// GET /users/:userId
+const getUserById = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(carId)) {
+    return res.status(400).json({ message: "Invalid user ID" });
+  }
+
+  try {
+    const user = await User.findById(userId);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve user" });
+  }
+};
+
+// PUT /users/:userId
+const updateUser = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(carId)) {
+    return res.status(400).json({ message: "Invalid user ID" });
+  }
+
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: userId },
+      { ...req.body },
+      { new: true }
+    );
+    if (updatedUser) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update user" });
+  }
+};
+
+// DELETE /users/:userId
+const deleteUser = async (req, res) => {
+  const { userId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(carId)) {
+    return res.status(400).json({ message: "Invalid user ID" });
+  }
+
+  try {
+    const deletedUser = await User.findOneAndDelete({ _id: userId });
+    if (deletedUser) {
+      res.status(200).json({ message: "User deleted successfully" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete user"});
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
---
+++ routes/blogRouter.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog,
+  // patchBlog
+} = require("../controllers/blogControllers");
+ 
+// GET /blogs
+router.get("/", getAllBlogs);
+
+// POST /blogs
+router.post("/", createBlog);
+
+// GET /blogs/:blogId
+router.get("/:blogId", getBlogById);
+
+// PUT /blogs/:blogId
+router.put("/:blogId", updateBlog);
+
+// DELETE /blogs/:blogId
+router.delete("/:blogId", deleteBlog);
+
+// Update blog using PATCH 
+// router.patch('/:blogId', patchBlog)
+
+module.exports = router;
